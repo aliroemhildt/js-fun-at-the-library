04b52157c006cb9f2d3ca734f1559f7925ddea12
Ali Roemhildt <aliroemhildt@gmail.com>
2021-10-16 15:11:23 -0600

    Refactor: change var names of calculatePageCount function

--- src/book.js
+++ src/book.js
@@ -16,17 +16,17 @@ function saveReview(review, reviewsArray) {
   for (i = 0; i < reviewsArray.length; i++) {
     if (review === reviewsArray[i]) {
       alreadyInArray = true;
-    }
-  }
+    };
+  };
   if (alreadyInArray === false) {
     reviewsArray.push(review);
-  }
+  };
 };
 
 function calculatePageCount(title) {
-  var letters = title.length;
-  pages = letters * 20;
-  return pages;
+  var numTitleLetters = title.length;
+  numPages = numTitleLetters * 20;
+  return numPages;
 };
 
 function writeBook(bookTitle, bookCharacter, genre) {
