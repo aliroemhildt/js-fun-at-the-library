ce83a892e892923786d12956374b781b64a261d4
Ali Roemhildt <aliroemhildt@gmail.com>
2021-10-17 10:01:20 -0600

    Pass second checkoutBook test

--- src/Library.js
+++ src/Library.js
@@ -16,12 +16,19 @@ function addBook(library, book) {
 };
 
 function checkoutBook(library, bookTitle, genre) {
+  var message = "";
+  var inShelf = false;
   for (i = 0; i < library.shelves[genre].length; i++) {
     if (library.shelves[genre][i].title === bookTitle) {
       library.shelves[genre].splice(i,1);
-     };
-   };
-   return `You have now checked out ${bookTitle} from the ${library.name}`;
+      message = `You have now checked out ${bookTitle} from the ${library.name}`;
+      inShelf = true;
+    };
+  };
+  if (inShelf === false) {
+    message = `Sorry, there are currently no copies of ${bookTitle} available at the ${library.name}`;
+  };
+  return message;
 };
 
 module.exports = {
